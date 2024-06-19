a96180be037d0f3b1cb264d660d0a4e311488642
riaz-khan-16 <goriazmte@gmail.com>
2024-06-20 00:06:09 +0600

    6.20.2024=> 352+2=354  Total spent time 354 Hours

--- Dynamic_Programming/Leetcode/7.decode_ways.py
+++ Dynamic_Programming/Leetcode/7.decode_ways.py
@@ -1,2 +1,30 @@
-# https://leetcode.com/problems/decode-ways/description/
+#https://leetcode.com/problems/decode-ways/description/
+
+#recursive solution
+
+
+s='226' #it is the given string
+memo={}
+def rec(ans,i):
+    if i in memo:
+        return memo[i]
+    
+    if i>=len(s):
+        return 1
+    if s[i]=='0':
+        return 0
+    x=rec(ans+[s[i]],i+1)
+
+    if i<len(s)-1 and int(s[i]+s[i+1])<=26 and int(s[i])>0:
+      x=x+rec(ans+[s[i]+s[i+1]],i+2)
+
+    memo[i]=x   
+    return x  
+print(rec([],0))
+
+
+
+
+
+
 
