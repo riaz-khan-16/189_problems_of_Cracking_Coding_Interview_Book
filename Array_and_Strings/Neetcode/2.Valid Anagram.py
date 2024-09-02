c95878f4c88976752109de1a2ae2567589511291
riaz-khan-16 <goriazmte@gmail.com>
2024-09-02 13:36:07 +0600

    array and hashmap started

---
+++ Array_and_Strings/Neetcode/2.Valid Anagram.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/valid-anagram/description/
+
+def solution(s,t):
+    hashs={}
+    x=set(s)
+    for i in x:
+        hashs[i]=s.count(i)
+    y=set(t)
+    hasht={}
+    for i in y:
+        hasht[i]=t.count(i)
+    for k in x:
+        if k not in y:
+            return False
+        if hashs[k]!=hasht[k]:
+            return False
+    return True
